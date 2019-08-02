3a5fda344e82c31d835683da65b88504ad39adea
Sipo Charles <sipocharles@yahoo.co.uk>
2019-08-02 01:55:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+<h1 align="center">
+<br>
+  <img src="static/img/veggit-1.png" width="600">
+  <br>
+    <br>
+  Online Cookbook
+  <br>
+</h1>
+
+<h4 align="center">A web application that allows users to store and easily access cooking recipes.</h4>
+
+This project is a fictional service, based on a real-world application, which offers a practical user-experience.
+
+## Table of Contents
+
+<!--ts-->
+
+- [About](#About)
+
+  - [Goal](#Goal)
+  - [Functionality (User Stories)](#Functionality-User-Stories)
+  - [Initiation](#Initiation)
+
+- [UX](#UX)
+
+  - [Layout Pro](#Layout-Pro-Boundless-Adaptability)
+  - [Layout Con](#Layout-Con-Moderate-Speed-and-Execution)
+  - [Tablet Display](#Tablet-Display)
+  - [Mobile Display](#Mobile-Display)
+  - [Additional Note](#Additional-Note)
+  - [Colour Scheme](#Colour-Scheme)
+  - [Font](#Font)
+  - [Navigation](#Navigation)
+
+- [Technologies](#Technologies)
+
+  - [Languages Frameworks Tools](#Languages-Frameworks-Tools)
+  - [Other-Resources](#Other-Resources)
+
+- [Features](#Features)
+
+  - [Existing Features](#Existing-Features)
+  - [Features-Left-to-Implement](#Features-Left-to-Implement)
+
+- [Testing](#Testing)
+
+  - [Tools-and-Methods-Used-for-Testing](#Tools-and-Methods-Used-for-Testing)
+  - [Additional-Points](#Additional-Points)
+  - [Tested Sections 1](#Tested-Sections-1)
+  - [Tested Sections 2](#Tested-Sections-2)
+
+- [Deployment](#Deployment)
+
+  - [How the project got deployed to Heroku](#How-the-project-got-deployed-to-Heroku)
+  - [Cloning the repository](#Cloning-the-repository)
+  - [How to access the live application](#How-to-access-the-live-application)
+  - [How to run things locally](#How-to-run-things-locally)
+
+- [Credits](#Credits)
+
+  - [Content](#Content)
+  - [Acknowledgements](#Acknowledgements)
+    <!--te-->
---
+++ run.py
@@ -0,0 +1,252 @@
+import os
+import math
+import datetime
+from flask import Flask
+from flask import render_template, url_for, request, session, redirect, jsonify
+from flask_pymongo import PyMongo, pymongo
+from bson.objectid import ObjectId
+
+# TODO: Add remaining docstrings to all the functions
+# TODO: Double check the HTML templates for formatting issues
+# TODO: Add classes to correct card styles on recipes and account pages
+# TODO: Add a message on the account page for when users have no recipes of their own.
+#       Use {% if count == 0%} and flexbox styles
+
+app = Flask(__name__)
+app.secret_key = 'any random string'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI') or "mongodb+srv://root:r00tUser@cluster0-0oagu.gcp.mongodb.net/recipebook?retryWrites=true"
+mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+    '''Retrieves four most viewed recipes in the recipes collection of database.
+    Renders the template for the homepage and provides the recipe information for the template.'''
+
+    recipes = list(mongo.db.recipes.find().sort("views", pymongo.DESCENDING).limit(4))
+    return render_template('index.html', title='Home | Veggit', recipes=recipes)
+
+
+@app.route('/recipes/', methods=['GET', 'POST'])
+@app.route('/recipes/<page>/show/<limit>', methods=['GET', 'POST'])
+def recipes(page=1, limit=12):
+    '''Limits the number of recipes shown on a page to a certain number. 
+    Generates pagination dynamically based on the number of records in the database.'''
+    limit = int(limit)
+
+    if request.method == 'POST':
+        limit = int(request.form['limit'])
+    
+    page = int(page)
+    skip = page * limit - limit
+    maximum = math.ceil( (mongo.db.recipes.count_documents({})) / limit)
+
+    recipes = list(mongo.db.recipes.find().sort("views", pymongo.DESCENDING).skip(skip).limit(limit))
+    return render_template(
+        'recipes.html',
+        title='Recipes | Veggit',
+        recipes=recipes,
+        page=page,
+        pages=range(1, maximum + 1),
+        maximum=maximum, limit=limit
+    )
+
+
+@app.route('/view/recipe_id?=<id>')
+def view(id):
+    '''Controls behavior of user-views increment operator.'''
+
+    mongo.db.recipes.find_one_and_update({"_id": ObjectId(id)}, {"$inc": {"views": 1}})
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(id)})
+    return render_template('view.html', title='View Recipe | Veggit', recipe=recipe)
+
+
+@app.route('/like/recipe_id?=<id>')
+def like(id):
+    '''Controls behavior of user-like increment and decrements operator.
+    Feature is dependant upon user interaction in the user-interface.'''
+
+    mongo.db.recipes.find_one_and_update({"_id": ObjectId(id)}, {"$inc": {"likes": 1}})
+    return redirect(url_for('recipes'))
+
+
+@app.route('/dislike/recipe_id?=<id>')
+def dislike(id):
+    '''Controls behavior of user-dislike increment and decrements operator.
+    Feature is dependant upon user interaction in the user-interface.'''
+    
+    mongo.db.recipes.find_one_and_update({"_id": ObjectId(id)}, {"$inc": {"dislikes": 1}})
+    return redirect(url_for('recipes'))
+
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    # check for logged in user
+    email = session.get('email')
+    if not email:
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        # get current date and time
+        now = datetime.datetime.utcnow()
+        current_date = datetime.datetime.strftime(now, '%b %d, %Y')
+        current_time = now.strftime("%I:%M %p")
+
+        recipe = request.get_json()
+
+        if recipe:
+            recipe.update(
+                email=email,
+                views=0,
+                likes=0,
+                dislikes=0,
+                time=current_time,
+                date=current_date
+            )
+            inserted = mongo.db.recipes.insert_one(recipe)
+            return redirect(url_for('view', id=inserted.inserted_id))
+
+    return render_template('create.html', title='Create Recipe | Veggit')
+
+
+@app.route('/edit/recipe_id?=<id>', methods=['GET', 'POST'])
+def edit(id):
+    # check for logged in user
+    email = session.get('email')
+    if not email:
+        return redirect(url_for('register'))
+    
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(id)})
+
+    if request.method == 'POST':
+        update_recipe = request.get_json()
+
+        if update_recipe:
+            update_recipe.update(
+                {
+                    'email': email,
+                    'views': recipe.get('views'),
+                    'likes': recipe.get('likes'),
+                    'dislikes': recipe.get('dislikes'),
+                    'time': recipe.get('time'),
+                    'date': recipe.get('date')
+                }
+            )
+
+            mongo.db.recipes.replace_one({"_id": ObjectId(id)}, update_recipe)
+            return redirect(url_for('view', id=id))
+
+    return render_template('edit.html', title='Edit Recipe | Veggit', recipe=recipe)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    # check for logged in user
+    email = session.get('email')
+    if email:
+        return redirect(url_for('register'))
+
+    user = None
+    if request.method == 'POST':
+        email = request.form["email"]
+        user = mongo.db.users.find_one({"email": email})
+
+        try:
+            assert(user["password"] == request.form["password"])
+        except (AssertionError, TypeError):
+            return render_template('login.html', title='Login | Veggit', user=None, error="incorrect_login")
+        else:
+            try:
+                session['name'] = user['name']
+            except KeyError:
+                session['name'] = 'John Doe'
+            session['email'] = email
+            return redirect(url_for("index"))
+
+    return render_template('login.html', title='Login | Veggit', user=user)
+
+
+@app.route('/delete/recipe_id?=<id>')
+def delete(id):
+    # check for logged in user
+    email = session.get('email')
+    if not email:
+        return redirect(url_for('register'))
+    try:
+        mongo.db.recipes.delete_one({"_id": ObjectId(id), 'email': email})
+    except:
+        return redirect(url_for('recipes'))
+    return redirect(url_for('account'))
+
+
+@app.route('/logout')
+def logout():
+    #  logout user and clear session
+    session['email'] = None
+    session['name'] = None
+    # session.clear()
+    return redirect(url_for('login'))
+
+
+@app.route('/account/')
+@app.route('/account/<page>')
+def account(page=1):
+    # check for logged in user
+    email = session.get('email')
+    if not email:
+        return redirect(url_for('register'))
+
+    limit = 12
+    page = int(page)
+    skip = page * limit - limit
+    maximum = math.ceil(mongo.db.recipes.count_documents({"email": email}) / 12)
+    count = mongo.db.recipes.find({ "email": email }).count()
+    recipes = list(mongo.db.recipes.find({"email": email}).sort("$natural", pymongo.DESCENDING).skip(skip).limit( 12 ))
+    
+    return render_template(
+        'account.html',
+        title='My Account | Veggit',
+        recipes=recipes,
+        page=page,
+        pages=range(1, maximum + 1),
+        maximum=maximum,
+        count=count
+    )
+
+@app.route('/signup', methods=['POST', 'GET'])
+def register():
+    '''Accepts POST and GET requests.
+
+    If the request is a GET request, checks for a session email.
+    If session email is found, the homepage is rendered.
+    If no session email is found, the register template is rendered.
+
+    If the request is a POST request, form data is retrieved.
+    A check is performed to verify that the user email doesn't already exist.
+    If the user already exists, the register page is rendered with a user_exists error.
+
+    If the user does not exist, the user is added to the database and the login page is rendered.'''
+
+    # check for logged in user
+    email = session.get('email')
+    if email:
+      return redirect(url_for('index'))
+
+    user = None
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        user = {'name': name, 'email': email, 'password': password}
+
+        if mongo.db.users.find_one({"email": email}):
+            return render_template('register.html', title='Sign up | Veggit', error="user_exists")
+        else:
+            mongo.db.users.insert_one(user)
+            return render_template('login.html', title='Login | Veggit', user=user, password=password)
+
+    return render_template('register.html', title='Sign up | Veggit')
+
+
+# Run App
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True, use_reloader=False)
