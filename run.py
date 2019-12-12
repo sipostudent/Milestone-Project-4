dae3de968d7e5f35a0946d44824c96aed8e4f45b
Sipo C. Charles <40184191+sipostudent@users.noreply.github.com>
2019-12-12 19:00:24 +0000

    Update run.py

--- run.py
+++ run.py
@@ -250,4 +250,4 @@ def register():
 # Run App
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
-            port=os.environ.get('PORT', '5000'))
+            port=os.environ.get('PORT', '5000'), debug=True)
